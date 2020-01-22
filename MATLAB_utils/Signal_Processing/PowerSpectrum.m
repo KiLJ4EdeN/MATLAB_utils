220a1d4ca2fd75449051cb65213a36f07ee24848
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-22 15:03:05 +0330

    Update PowerSpectrum.m

--- MATLAB_utils/Signal_Processing/PowerSpectrum.m
+++ MATLAB_utils/Signal_Processing/PowerSpectrum.m
@@ -1,9 +1,14 @@
-function out=PowerSpectrum(data)
-data=data(3,34000:36000);
- % Apply wavelet denoising
+function out = PowerSpectrum(data)
+
+
+ % Apply wavelet denoising (Optional)
 data=wden(data,'rigrsure','s', 'sln',4,'db2');
+
+ % perform welchs average periodogram method
 [pxx f]=pwelch(data,[],[],[],256);
+
  % Find the desired frequency
+ 
 deltaIdx = find(f>=1 & f<=4);
 thetaIdx = find(f>=4 & f<=8);
 alphaIdx = find(f>=8 & f<=13);
@@ -11,13 +16,14 @@ betaIdx  = find(f>=13 & f<=30);
 gammaIdx = find(f>=30 & f<=50);
  
  % Compute power.
- deltaPower = mean(10*log10(pxx(deltaIdx)));
- thetaPower = mean(10*log10(pxx(thetaIdx)));
- alphaPower = mean(10*log10(pxx(alphaIdx)));
- betaPower  = mean(10*log10(pxx(betaIdx)));
- beta1Power  = mean(10*log10(pxx(beta1)));
- beta2Power  = mean(10*log10(pxx(beta2)));
- gammaPower = mean(10*log10(pxx(gammaIdx)));
+deltaPower = mean(10*log10(pxx(deltaIdx)));
+thetaPower = mean(10*log10(pxx(thetaIdx)));
+alphaPower = mean(10*log10(pxx(alphaIdx)));
+betaPower  = mean(10*log10(pxx(betaIdx)));
+beta1Power  = mean(10*log10(pxx(beta1)));
+beta2Power  = mean(10*log10(pxx(beta2)));
+gammaPower = mean(10*log10(pxx(gammaIdx)));
 
+# or any power you are working on. possibly you can get all of the arrays as out if needed.
 out=deltaPower;
 end
