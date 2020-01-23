0c68f7e4ab2aac76166a1086bb555d5eb6f69515
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-23 11:44:33 +0330

    Update and rename mvingavgFilter.m to Moving_Average_Filter.m

---
+++ MATLAB_utils/Signal_Processing/Moving_Average_Filter.m
@@ -0,0 +1,6 @@
+function out = Moving_Average_Filter(data, size)
+n = 1:length(data);
+b = (1/size)*ones(1,size);
+a = 1;
+out = filter(b, a, data);
+end
--- MATLAB_utils/Signal_Processing/mvingavgFilter.m
+++
@@ -1,7 +0,0 @@
-function abdol=digitalFilter(data)
-    t=1:length(data);
-b=(1/5000)*ones(1,5000);
-a=1;
-y=filter(b,a,data);
-abdol=y;
-end
