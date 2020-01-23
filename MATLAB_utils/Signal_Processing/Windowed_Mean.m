9ffba451b08639bb5973b10588089999829cd694
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-23 11:46:46 +0330

    Update and rename WinMean.m to Windowed_Mean.m

--- MATLAB_utils/Signal_Processing/Windowed_Mean.m
+++ MATLAB_utils/Signal_Processing/Windowed_Mean.m
@@ -1,5 +1,5 @@
-# absolute windowed detrentding.
-function out=WinMean(data, win_size)
+% absolute windowed detrentding.
+function out=Windowed_Mean(data, win_size)
     for j = 1:win_size:length(data) - win_size
         temp(j) = mean(abs(data(j:j+win_size) - mean(data(j:j+win_size))));
     end
