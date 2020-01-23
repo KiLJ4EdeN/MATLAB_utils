fd227206e1ba68f8f041baca8347790634b217fc
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-23 11:43:06 +0330

    Update and rename WinMeaN.m to WinMean.m

--- MATLAB_utils/Signal_Processing/WinMeaN.m
+++
@@ -1,6 +0,0 @@
-function abdol=karim(ld3)
-    for j=1:35:length(ld3)-35
-   an(j)=mean(abs(ld3(j:j+35)-mean(ld3(j:j+35))));
-    end
-    abdol=mean(an(1:35:end));
-end
---
+++ MATLAB_utils/Signal_Processing/WinMean.m
@@ -0,0 +1,7 @@
+# absolute windowed detrentding.
+function out=WinMean(data, win_size)
+    for j = 1:win_size:length(data) - win_size
+        temp(j) = mean(abs(data(j:j+win_size) - mean(data(j:j+win_size))));
+    end
+    out=mean(temp(1:win_size:end));
+end
