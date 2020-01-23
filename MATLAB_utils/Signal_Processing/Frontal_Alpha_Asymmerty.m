ffe6b0a0c69c7d59fdf593f8a0e0e0d7d3edf641
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-23 11:47:08 +0330

    Update Frontal_Alpha_Asymmerty.m

--- MATLAB_utils/Signal_Processing/Frontal_Alpha_Asymmerty.m
+++ MATLAB_utils/Signal_Processing/Frontal_Alpha_Asymmerty.m
@@ -1,24 +1,24 @@
 function out = Frontal_Alpha_Asymmetry(data, channel1, channel2, fs)
 
-# exctracting the channels
+% exctracting the channels
 
 data1=data(channel1, :);
 data2=data(channel2, :);
 
-# wavelet denoising (can be removed)
+% wavelet denoising (can be removed)
 data=wden(data,'rigrsure','s', 'sln',5,'db2');
 [pxx1 f1]=pwelch(data1,[],[],[], fs);
 [pxx2 f2]=pwelch(data2,[],[],[], fs);
 
-# find alpha rythm
+% find alpha rythm
 alphaIdx1 = find(f1>=8 & f1<=13);
 alphaIdx2 = find(f2>=8 & f2<=13);
 
-# calculating alpha power in each channel
+% calculating alpha power in each channel
 asymmetryLeft=mean(10*log10(pxx1(alphaIdx1)));
 asymmetryRight=mean(10*log10(pxx2(alphaIdx2)));
 
-# computing asymmetry
+% computing asymmetry
 out=abs((asymmetryLeft)-(asymmetryRight));
 
 end
