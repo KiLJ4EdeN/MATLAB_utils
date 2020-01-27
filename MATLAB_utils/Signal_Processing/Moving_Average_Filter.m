1ce8091a973ad6b9b54205f3edd25e2940773121
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-27 08:34:02 +0330

    Update Moving_Average_Filter.m

--- MATLAB_utils/Signal_Processing/Moving_Average_Filter.m
+++ MATLAB_utils/Signal_Processing/Moving_Average_Filter.m
@@ -1,8 +1,13 @@
 % simple moving average filter
 
 function out = Moving_Average_Filter(data, size)
-n = 1:length(data);
+
+%  chooise the moving average filter size.
 b = (1/size)*ones(1,size);
+
+% the other side of the lccde in 1 in MA difference equations.
 a = 1;
+
+% apply the filter on the data.
 out = filter(b, a, data);
 end
